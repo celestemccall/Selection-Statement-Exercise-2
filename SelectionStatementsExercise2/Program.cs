a6ecf8315aa0d8806aa5214d9ad5b88b1ec8c528
Celeste McCall <celestemccall93@gmail.com>
2023-01-21 00:02:15 -0600

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            Console.WriteLine("Input your answer below.?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Nice, math is a not an easy subject for many!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Cool, science is a very experimental subject!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("Wonderful, english is a very useful subject!");
+                    break;
+
+                case "physical education":
+                    Console.WriteLine("Yay! physical education is an incredibly fun subject!");
+                    break;
+
+                case "social studies":
+                    Console.WriteLine("Great, social studies is a very interesting subject!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Wow, I have never taken that subject before! {subject} sounds fun!");
+                    break;
+
+               
+
+
+
+       
+            }
+
+
         }
     }
 }
